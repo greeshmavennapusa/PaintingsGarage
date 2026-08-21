0147435d1222c82160fcb763c5bcf36e0802ed57
greeshmavennapusa <greeshma.vennapusa@qualizeal.com>
2026-08-21 16:27:51 +0530

    Rename class from CoverageToJson to JacocoToJson

--- ci/coverage/JacocoToJson.java
+++ ci/coverage/JacocoToJson.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public final class CoverageToJson {
+public final class JacocoToJson {
 
   private static final String SOURCE_ROOT = "src/main/java";
 
